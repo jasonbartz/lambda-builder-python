ee8567ef821e41b77a032ebc9ad335b9aa5d7bb0
Jason Bartz <jason.bartz@washpost.com>
2017-02-06 13:39:00 -0500

    update config

--- builder/run.sh
+++ builder/run.sh
@@ -4,8 +4,12 @@
 : ${LAMBDA_ROLE?"LAMBDA_ROLE must be set."}
 : ${LAMBDA_HANDLER?"LAMBDA_HANDLER must be set."}
 
-pip install -t /lambda -r /data/requirements.txt && cd /lambda && zip -r lambda.zip *
-cd /data && zip -r /lambda/lambda.zip *
+pip install --target /lambda -r /data/requirements.txt && cd /lambda && zip -r /data/lambda.zip *
+fail=$?
+if [ $fail -eq 1 ];then
+    exit 1
+fi
+cd /data && zip -r /data/lambda.zip *
 
 aws lambda get-function --function-name "${NAME}"
 exists=$?
@@ -16,9 +20,13 @@ if [ $exists -eq 255 ];then
         --runtime python2.7 \
         --role ${LAMBDA_ROLE} \
         --handler ${LAMBDA_HANDLER} \
-        --zip-file fileb:///lambda/lambda.zip
+        --zip-file fileb:///data/lambda.zip
 else
     aws lambda update-function-code \
         --function-name "${NAME}" \
-        --zip-file fileb:///lambda/lambda.zip
+        --zip-file fileb:///data/lambda.zip
+    aws lambda update-function-configuration \
+        --function-name "${NAME}" \
+        --role ${LAMBDA_ROLE} \
+        --handler ${LAMBDA_HANDLER}
 fi
