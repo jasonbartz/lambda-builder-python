ce995203b35317a2b3c4bdd2bd229364cbdadcfd
Jason Bartz <jason.bartz@washpost.com>
2017-02-06 17:47:03 -0500

    latest config and examples

---
+++ builder/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t jasonbartz/lambda-builder-python .
+docker push jasonbartz/lambda-builder-python
--- builder/run.sh
+++ builder/run.sh
@@ -4,7 +4,7 @@
 : ${LAMBDA_ROLE?"LAMBDA_ROLE must be set."}
 : ${LAMBDA_HANDLER?"LAMBDA_HANDLER must be set."}
 
-pip install --target /lambda -r /data/requirements.txt && cd /lambda && zip -r /data/lambda.zip *
+pip install --upgrade --target /lambda -r /data/requirements.txt && cd /lambda && zip -r /data/lambda.zip *
 fail=$?
 if [ $fail -eq 1 ];then
     exit 1
@@ -13,20 +13,27 @@ cd /data && zip -r /data/lambda.zip *
 
 aws lambda get-function --function-name "${NAME}"
 exists=$?
+
+CONFIG="--role ${LAMBDA_ROLE}"
+CONFIG="$CONFIG --handler ${LAMBDA_HANDLER}"
+
+if [ ! -z ${VPC_CONFIG} ];then
+    CONFIG="$CONFIG --vpc-config $VPC_CONFIG"
+fi
+
 if [ $exists -eq 255 ];then
     echo $exists
     aws lambda create-function \
         --function-name "${NAME}" \
         --runtime python2.7 \
-        --role ${LAMBDA_ROLE} \
-        --handler ${LAMBDA_HANDLER} \
+        $CONFIG \
         --zip-file fileb:///data/lambda.zip
 else
     aws lambda update-function-code \
         --function-name "${NAME}" \
         --zip-file fileb:///data/lambda.zip
+
     aws lambda update-function-configuration \
         --function-name "${NAME}" \
-        --role ${LAMBDA_ROLE} \
-        --handler ${LAMBDA_HANDLER}
+        $CONFIG
 fi
